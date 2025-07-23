bc6e1925f370aa878e6fc9aba6d978632ba8489c
ToneBone54 <austin.tone007@gmail.com>
2025-07-23 18:38:21 +0000

    Added a secret response

--- Python/catgpt/main.py
+++ Python/catgpt/main.py
@@ -7,9 +7,14 @@ meow_multiplier = int(r.randint(1, 20))
 # Random number of seconds 
 sleepTimer = int(r.randint(5, 25)) 
 
+surprise = int(r.randint(1, 4))
+
 input("What would you like to ask?: ") 
 
 # Time in seconds
 time.sleep(sleepTimer) 
 
-print("Meow " * meow_multiplier)
+if surprise > 3:
+    print("I can haz cheezburger")
+else:
+    print("Meow " * meow_multiplier)
