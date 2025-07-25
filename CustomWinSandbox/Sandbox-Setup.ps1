938f8d284cdc75636424038cfa5f6570b7f3094c
ToneBone54 <austin.tone007@gmail.com>
2025-07-25 16:03:04 +0000

    Added "Setup Complete" message box to custom WSB

--- CustomWinSandbox/Sandbox-Setup.ps1
+++ CustomWinSandbox/Sandbox-Setup.ps1
@@ -2,3 +2,7 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 choco install thunderbird sharex -y
+
+# Set up presentation type to display message boxes
+Add-Type -AssemblyName PresentationCore,PresentationFramework
+[System.Windows.MessageBox]::Show('Sandbox Setup complete!', 'Sandbox Setup', 'OK', 'Information')
