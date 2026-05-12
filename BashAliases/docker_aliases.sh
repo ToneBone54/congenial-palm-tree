916dcf7189911f5cd6c9fbc65a82a5b103c8779d
ToneBone <austin.tone007@gmail.com>
2026-05-11 19:51:14 -0500

    Update docker_aliases.sh
    Cleans up output

--- BashAliases/docker_aliases.sh
+++ BashAliases/docker_aliases.sh
@@ -3,4 +3,5 @@ echo -n "alias dps='docker ps'
 alias drm='docker rm'
 alias dimage='docker image'
 alias dcompose='docker compose'
-alias drun='docker run'" >> ~/.bash_aliases
+alias drun='docker run'
+" >> ~/.bash_aliases
